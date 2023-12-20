fc0bfee2fe832cd0d8d73f2fa68361e387da5278
VAshish07243 <151644370+VAshish07243@users.noreply.github.com>
2023-12-20 22:13:45 +0530

    Update addTodo.js

--- todo-cli/addTodo.js
+++ todo-cli/addTodo.js
@@ -20,7 +20,7 @@ const getJSDate = (days) => {
 (async () => {
   const { title, dueInDays } = argv;
   if (!title || dueInDays === undefined) {
-    throw new Error("title and dueInDays are required. \nSample command: node addTodo.js --title=\"Buy milk\" --dueInDays=-2 ")
+    throw new Error("title and dueInDays are required. \nsample command: node addTodo.js --title=\"Buy milk\" --dueInDays=-2 ")
   }
   await createTodo({ title, dueDate: getJSDate(dueInDays), completed: false })
   await db.Todo.showList();
